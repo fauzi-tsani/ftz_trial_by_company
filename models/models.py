d9f5bbd26a1f0077f2618c2b07da1feb6e95320c
fauzi.tsani <fauzi.tsani@wgs.co.id>
2017-12-01 12:41:18 +0700

    fix error and bug

--- models/models.py
+++ models/models.py
@@ -2,14 +2,16 @@
 
 from odoo import models, fields, api
 from odoo.exceptions import UserError, ValidationError
+from odoo.tools.translate import _
 
 LIMIT_USER = 5
 class res_users(models.Model):
     _inherit = 'res.users'
     # _description = 'Description'
 
-    @api.models
+    @api.model
     def create(self,vals):
     	users_ids = self.search([])
     	if len(users_ids) > LIMIT_USER:
-    		raise UserError(_('You cannot create more user'))
+    		raise UserError(_('You cannot create more than 5 user'))
+    	return super(res_users,self).create(vals)
